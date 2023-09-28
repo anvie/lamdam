696cb14015cfd5145946b98f7ccc35b35ecaf89b
anvie <robinsyihab@gmail.com>
2023-09-28 16:51:44 +0700

    increase max_tokens limitation for LLMResponseView

--- components/LLMResponseView.tsx
+++ components/LLMResponseView.tsx
@@ -109,7 +109,7 @@ const LLMResponseView: FC<Props> = ({
       temperature: 0.01,
       top_p: 0,
       n: 1,
-      max_tokens: 1024,
+      max_tokens: 2000,
       stream: true,
     };
     const requestOptions: RequestInit = {
