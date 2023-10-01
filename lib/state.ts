af196b0c250a960533e43b44262191acc01072c1
Binsar Dwi Jasuma <binsarjr121@gmail.com>
2023-10-01 16:53:25 +0700

    add useLocalStorage state

---
+++ lib/state.ts
@@ -0,0 +1,20 @@
+import { useEffect, useState } from 'react'
+
+export const useLocalStorage = <T>(key: string, initialValue: T) => {
+    const state = useState<T>(() => {
+        try {
+            const item = localStorage.getItem(key)
+            return item ? JSON.parse(item) : initialValue
+        } catch {
+            return initialValue
+        }
+    })
+
+
+    useEffect(() => {
+        localStorage.setItem(key, JSON.stringify(state[0]))
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [state[0], key])
+
+    return state
+}
