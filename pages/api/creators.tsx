a59e162f063e551e1f8a700a546cd65099b493ec
anvie <robinsyihab@gmail.com>
2023-10-05 23:05:51 +0700

    Fix indentation after merge with feature/search-setting

--- pages/api/creators.tsx
+++ pages/api/creators.tsx
@@ -1,31 +1,28 @@
-import { apiHandler } from "@/lib/ApiHandler"
-import { Collection } from "@/models/Collection"
-import type { NextApiRequest, NextApiResponse } from "next/types"
-
+import { apiHandler } from "@/lib/ApiHandler";
+import { Collection } from "@/models/Collection";
+import type { NextApiRequest, NextApiResponse } from "next/types";
 
 const db = require("../../lib/db");
 
 async function handler(req: NextApiRequest, res: NextApiResponse<string[]>) {
-	const {
-		method,
-	} = req;
+    const { method } = req;
 
-	if (method !== "GET") {
-		res.status(405).end();
-		return;
-	}
+    if (method !== "GET") {
+        res.status(405).end();
+        return;
+    }
 
-	const items = await Collection.find();
-	const creators: string[] = [];
+    const items = await Collection.find();
+    const creators: string[] = [];
 
-	for (const { creator } of items) {
-		if (!creators.includes(creator)) creators.push(creator);
-	}
-	return res.json(creators);
+    for (const { creator } of items) {
+        if (!creators.includes(creator)) creators.push(creator);
+    }
+    return res.json(creators);
 
-	// return await DataRecordRow.find({}).sort({createdAt:-1}).then((docs: any[]) => {
-	//   return res.json({ result: docs.map(toApiRespDoc) });
-	// });
+    // return await DataRecordRow.find({}).sort({createdAt:-1}).then((docs: any[]) => {
+    //   return res.json({ result: docs.map(toApiRespDoc) });
+    // });
 }
 
 export default apiHandler(handler);
--- pages/api/records.tsx
+++ pages/api/records.tsx
@@ -1,122 +1,125 @@
-import { apiHandler } from "@/lib/ApiHandler"
-import { toApiRespDoc } from "@/lib/docutil"
-import { DataRecord } from "@/types"
-import type { NextApiRequest, NextApiResponse } from "next/types"
+import { apiHandler } from "@/lib/ApiHandler";
+import { toApiRespDoc } from "@/lib/docutil";
+import { DataRecord } from "@/types";
+import type { NextApiRequest, NextApiResponse } from "next/types";
 
 // import db from "@/lib/db";
 const db = require("../../lib/db");
 const mongoose = require("mongoose");
 
-import { __error } from "@/lib/logger"
-import { Collection } from "@/models/Collection"
-import { Types } from "mongoose"
+import { __error } from "@/lib/logger";
+import { Collection } from "@/models/Collection";
+import { Types } from "mongoose";
 
 type Data = {
-	error?: string;
-	result?: DataRecord[];
+    error?: string;
+    result?: DataRecord[];
 };
 
 async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
-	// immutable
-	const {
-		query: { collectionId, q, order, fromId, toId },
-		method,
-	} = req;
+    // immutable
+    const {
+        query: { collectionId, q, order, fromId, toId },
+        method,
+    } = req;
 
-	// muttable
-	let {
-		query: { creators = [], features = ["prompt"] },
-	} = req;
+    // muttable
+    let {
+        query: { creators = [], features = ["prompt"] },
+    } = req;
 
-	if (method !== "GET") {
-		res.status(405).end();
-		return;
-	}
+    if (method !== "GET") {
+        res.status(405).end();
+        return;
+    }
 
-	const colDoc = await Collection.findOne({ _id: collectionId });
+    const colDoc = await Collection.findOne({ _id: collectionId });
 
-	if (!colDoc) {
-		return res.status(404).end();
-	}
-	const col = mongoose.connection.db.collection(colDoc.name);
+    if (!colDoc) {
+        return res.status(404).end();
+    }
+    const col = mongoose.connection.db.collection(colDoc.name);
 
-	creators = Array.isArray(creators) ? creators : [creators];
-	features = Array.isArray(features) ? features : [features];
+    creators = Array.isArray(creators) ? creators : [creators];
+    features = Array.isArray(features) ? features : [features];
 
-	let query: any = {};
+    let query: any = {};
 
-	if (q) {
-		let $and: any[] = [];
+    if (q) {
+        let $and: any[] = [];
 
-		if (features.length) {
-			let featureQuery: any[] = [];
+        if (features.length) {
+            let featureQuery: any[] = [];
 
-			for (const feature of features) {
-				featureQuery.push({ [feature]: { $regex: q, $options: "i" } });
-			}
-			$and.push({
-				$or: featureQuery,
-			});
-		}
+            for (const feature of features) {
+                featureQuery.push({ [feature]: { $regex: q, $options: "i" } });
+            }
+            $and.push({
+                $or: featureQuery,
+            });
+        }
 
-		if (creators.length) {
-			$and.push({
-				creator: { $in: creators },
-			});
-		}
-		query = {
-			...query,
-			$and: $and,
-		};
-		console.log(query);
-	}
+        if (creators.length) {
+            $and.push({
+                creator: { $in: creators },
+            });
+        }
+        query = {
+            ...query,
+            $and: $and,
+        };
+        console.log(query);
+    }
 
-	let sortOrder: any = { _id: -1 };
+    let sortOrder: any = { _id: -1 };
 
-	// if (order == "createdAt:1"){
-	//   sortOrder = { createdAt: 1 };
-	// }else if (order == "lastUpdated:1"){
-	//   sortOrder = { lastUpdated: 1 };
-	// }else if (order == "lastUpdated:-1"){
-	//   sortOrder = { lastUpdated: -1 };
-	// }
+    // if (order == "createdAt:1"){
+    //   sortOrder = { createdAt: 1 };
+    // }else if (order == "lastUpdated:1"){
+    //   sortOrder = { lastUpdated: 1 };
+    // }else if (order == "lastUpdated:-1"){
+    //   sortOrder = { lastUpdated: -1 };
+    // }
 
-	if (toId) {
-		query = { ...query, _id: { $lt: new Types.ObjectId(toId as string) } };
-	}
-	if (fromId) {
-		query = { ...query, _id: { $gt: new Types.ObjectId(fromId as string) } };
-		sortOrder = { _id: 1 };
-		// __debug('sortOrder:', sortOrder)
-	}
+    if (toId) {
+        query = { ...query, _id: { $lt: new Types.ObjectId(toId as string) } };
+    }
+    if (fromId) {
+        query = {
+            ...query,
+            _id: { $gt: new Types.ObjectId(fromId as string) },
+        };
+        sortOrder = { _id: 1 };
+        // __debug('sortOrder:', sortOrder)
+    }
 
-	// __debug('query:', query)
+    // __debug('query:', query)
 
-	return await col
-		.find(query)
-		.sort(sortOrder)
-		.limit(10)
-		.toArray()
-		.then((docs: any[]) => {
-			if (fromId) {
-				// reverse sort order
-				docs = docs.reverse();
-			}
-			return res.json({
-				result: docs.map(toApiRespDoc).map((rec) => {
-					rec.history = rec.history ? rec.history : [];
-					return rec;
-				}),
-			});
-		})
-		.catch((err: any) => {
-			__error("err:", err);
-			return res.status(404).json({ result: [] });
-		});
+    return await col
+        .find(query)
+        .sort(sortOrder)
+        .limit(10)
+        .toArray()
+        .then((docs: any[]) => {
+            if (fromId) {
+                // reverse sort order
+                docs = docs.reverse();
+            }
+            return res.json({
+                result: docs.map(toApiRespDoc).map((rec) => {
+                    rec.history = rec.history ? rec.history : [];
+                    return rec;
+                }),
+            });
+        })
+        .catch((err: any) => {
+            __error("err:", err);
+            return res.status(404).json({ result: [] });
+        });
 
-	// return await DataRecordRow.find({}).sort({createdAt:-1}).then((docs: any[]) => {
-	//   return res.json({ result: docs.map(toApiRespDoc) });
-	// });
+    // return await DataRecordRow.find({}).sort({createdAt:-1}).then((docs: any[]) => {
+    //   return res.json({ result: docs.map(toApiRespDoc) });
+    // });
 }
 
 export default apiHandler(handler);
